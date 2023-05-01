09ad770b49944bb184210a3bd2be34d9142c7750
Pakhaji30 <131833940+Pakhaji30@users.noreply.github.com>
2023-05-01 22:21:26 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/nanotechid/supreme/aio/permission/ip | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/Pakhaji30/supreme/aio/permission/ip | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
